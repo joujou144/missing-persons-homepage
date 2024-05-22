bfaca6596f7bce62517275ab4686345375845e0c
Aznim Rahman <aznimr@protonmail.com>
2024-05-22 17:55:26 +0100

    restyle navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,31 +6,34 @@ import { usePathname } from "next/navigation";
 import { LuUser2 } from "react-icons/lu";
 import React from "react";
 import { NAV_LINKS } from "@/constant";
-import classNames from "classnames";
+import clasnames from "classnames";
 import { IoMenuSharp } from "react-icons/io5";
 
 const Navbar = () => {
   return (
-    <nav className="z-40 bg-cream padding-container flexBetween py-6 absolute right-0 left-0">
-      <Link href="/">
-        <Image
-          src="/Logo-dark.svg"
-          alt="LostInNL-logo"
-          width={80}
-          height={30}
-        />
-      </Link>
-      <div className="max-md:hidden flex items-center gap-6">
-        <Navlinks />
-        {/* Add AuthStatus */}
+    <nav className="sticky max-w-[1350px] mx-auto z-40 bg-white bg-opacity-30 backdrop-filter backdrop-blur-lg right-0 left-0 top-4 rounded-full firefox:bg-opacity-90">
+      <div className="max-container py-6">
+        <div className="flexBetween mx-4">
+          <Link href="/">
+            <Image
+              src="/Logo-dark.svg"
+              alt="LostInNL-logo"
+              width={80}
+              height={30}
+            />
+          </Link>
+          <div className="max-md:hidden flex items-center gap-6">
+            <Navlinks />
+            {/* Add AuthStatus */}
 
-        <LuUser2 size={20} className="max-md:hidden block cursor-pointer" />
+            <LuUser2 size={20} className="max-md:hidden block cursor-pointer" />
+          </div>
+          <IoMenuSharp
+            size={25}
+            className="md:hidden inline-block cursor-pointer"
+          />
+        </div>
       </div>
-
-      <IoMenuSharp
-        size={25}
-        className="md:hidden inline-block cursor-pointer"
-      />
     </nav>
   );
 };
@@ -44,8 +47,8 @@ const Navlinks = () => {
         <li key={key}>
           <Link
             href={href}
-            // className={classNames({
-            //   "nav-link": true,
+            // className={classnames({
+            //   "links": true,
             //   "!text-shadow-sm cursor-pointer": href === currentPath,
             // })}
             className="links"
