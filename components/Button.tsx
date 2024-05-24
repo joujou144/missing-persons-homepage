3d0c74a1e336b63a2473f90cf862dc1af8923ac1
Aznim Rahman <aznimr@protonmail.com>
2024-05-24 22:07:04 +0100

    change button text

--- components/Button.tsx
+++ components/Button.tsx
@@ -22,7 +22,7 @@ const Button = ({
       type={type}
       disabled={disabled}
       onClick={onClick}
-      className={`${className} z-10 text-center flexCenter gap-1 cursor-pointer group relative inline-block overflow-hidden rounded-full border-2 border-cream px-5 py-2.5 font-medium text-midnight bg-cream disabled:text-gray-20 disabled:bg-gray-30 disabled:border-gray-30 disabled:cursor-wait`}
+      className={`${className} content-size z-10 text-center flexCenter gap-1 cursor-pointer group relative inline-block overflow-hidden rounded-full border-2 border-cream px-5 py-2.5 font-medium text-midnight bg-cream disabled:text-gray-20 disabled:bg-gray-30 disabled:border-gray-30 disabled:cursor-wait`}
     >
       <span className="absolute left-0 top-0 mb-0 flex h-full w-0 translate-x-0 transform bg-gray-200 opacity-90 transition-all duration-300 ease-out group-hover:w-full"></span>
       {icon && <PiPlusBold className="relative group-hover:text-cream" />}
